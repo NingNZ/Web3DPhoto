67c2225db6a76db778751e9593470ccd6d6bcb80
Ning_0 <ning_0@petalmail.com>
2024-04-24 16:31:28 +0800

    First Submit with the empty context

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import * as THREE from 'three'
+</script>
+
+<template>
+</template>
+<script>
+
+</script>
+
+<style scoped>
+</style>
